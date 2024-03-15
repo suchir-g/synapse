5bf2962d82460d05ecc7b4ff77a13b47dd51e093
dunkshotenthusiast <18sgupta@heckgrammar.co.uk>
2024-03-15 17:27:59 +0000

    merge

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -27,6 +27,7 @@ const Dashboard = ({ isAuth }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [hasRevisedToday, setHasRevisedToday] = useState(false);
   const [mainTodoListId, setMainTodoListId] = useState(null);
+  const [revisionSetsDueToday, setRevisionSetsDueToday] = useState([]);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(async (user) => {
@@ -38,8 +39,38 @@ const Dashboard = ({ isAuth }) => {
       }
     });
 
+    loadRevisionsDueToday();
     return () => unsubscribe();
   }, []);
+
+  const isRevisionDueToday = (revisionDates) => {
+    const today = new Date();
+    const dateString = `${today.getFullYear()}-${(today.getMonth() + 1)
+      .toString()
+      .padStart(2, "0")}-${today.getDate().toString().padStart(2, "0")}`;
+    return revisionDates.includes(dateString);
+  };
+
+  const loadRevisionsDueToday = async (userId) => {
+    const today = new Date();
+    const dateString = `${today.getFullYear()}-${(today.getMonth() + 1)
+      .toString()
+      .padStart(2, "0")}-${today.getDate().toString().padStart(2, "0")}`;
+
+    // Query revisionSchedules where today's date is within the revisionDates array
+    const revisionSchedulesQuery = query(
+      collection(db, "revisionSchedules"),
+      where(`revisionDates`, "array-contains", dateString)
+    );
+    const querySnapshot = await getDocs(revisionSchedulesQuery);
+
+    const revisionsDueToday = [];
+    querySnapshot.forEach((doc) => {
+      revisionsDueToday.push({ id: doc.id, ...doc.data() });
+    });
+
+    setRevisionSetsDueToday(revisionsDueToday);
+  };
 
   const checkRevisionStatus = (lastRevised) => {
     if (!lastRevised) return;
@@ -102,6 +133,14 @@ const Dashboard = ({ isAuth }) => {
     }
     setIsLoading(false);
   };
+  const formatFirestoreTimestamp = (timestamp) => {
+    if (!timestamp) return "";
+    const date = timestamp.toDate(); // Converts the Timestamp to a JavaScript Date object
+    const day = date.getDate().toString().padStart(2, "0"); // Ensures the day is two digits
+    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Months are 0-indexed, plus ensures two digits
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`; // Concatenates in "dd-mm-yyyy" format
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -120,16 +159,41 @@ const Dashboard = ({ isAuth }) => {
               </h3>
             </div>
             <section className={styles.contentSection}>
+              <div className={styles.revisionsDueTodaySection}>
+                <h2>Revisions Due Today</h2>
+                <div className={styles.gridContainer}>
+                  {revisionSetsDueToday.map((set) => (
+                    <div key={set.id} className={styles.card}>
+                      <div className={styles.cardBody}>
+                        <Link
+                          to={`/sets/${set.flashcardId}`}
+                          className={styles.cardLink}
+                        >
+                          <h3>Revision: {set.flashcardId}</h3>
+                        </Link>
+                        <p className={styles.cardDescription}>
+                          Number of Revisions: {set.numberOfRevisions}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
               <div className={styles.latestFlashcards}>
                 <h2 className={styles.latestText}>Latest Flashcard Sets</h2>
                 <div className={styles.gridContainer}>
                   {latestSets.map((set) => (
                     <div key={set.id} className={styles.card}>
                       <div className={styles.cardBody}>
-                        <Link to={`/sets/${set.id}`} className={styles.cardLink}>
+                        <Link
+                          to={`/sets/${set.id}`}
+                          className={styles.cardLink}
+                        >
                           <h3>{set.title}</h3>
                         </Link>
-                        <p className={styles.cardDescription}>{set.description}</p>
+                        <p className={styles.cardDescription}>
+                          {set.description}
+                        </p>
                       </div>
                       <div className={styles.cardFooter}>
                         <span className={styles.lastRevised}>
@@ -146,20 +210,23 @@ const Dashboard = ({ isAuth }) => {
                 <div className={styles.gridContainer}>
                   {latestNotes.map((note) => (
                     <div key={note.id} className={styles.card}>
-                      <div className={styles.cardMainSection}> 
-                        <Link to={`/notes/${note.id}`} className={styles.cardLink}>
+                      <div className={styles.cardMainSection}>
+                        <Link
+                          to={`/notes/${note.id}`}
+                          className={styles.cardLink}
+                        >
                           <h3>{note.title}</h3>
                         </Link>
                         <div
-                          className={`${styles.cardContent}`}
+                          className={`${styles.cardDescription}`}
                           dangerouslySetInnerHTML={{
-                            __html: sanitizeAndTruncateHtml(note.content),
+                            __html: sanitizeAndTruncateHtml(note.content, 20),
                           }}
                         />
                       </div>
                       <div className={styles.cardFooter}>
                         <span className={styles.lastRevised}>
-                          Last revised: {note.lastRevised}
+                          Last revised: {formatFirestoreTimestamp(note.viewed)}
                         </span>
                       </div>
                     </div>
@@ -187,7 +254,6 @@ const Dashboard = ({ isAuth }) => {
           <div className={styles.examCountdown}>
             <ExamCountdown />
           </div>
-
         </div>
       </div>
 
